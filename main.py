a5c93c61b877c9998ffd51d049f91ecde6c51fd0
Bryce Elizabeth Yahn <43454802+byahn2@users.noreply.github.com>
2020-11-08 20:58:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
 #Bryce will be responsible for preprocessing and coding/running SVM.
 #Jeremy will be responsible for: visualization, coding/running Bayes.
 #We will both be contributing to the presentation, report, proofreading, checking, helping each other debug.
@@ -10,7 +13,12 @@ def visualize_data(data):
 
 #DATA PREPROCESSING
 def preprocess(raw_data):
-    print('preprocess the data')
+    target_index = len(raw_data.columns)-1
+    attributes = raw_data.drop(target_index, axis=1)
+    target = raw_data[target_index]
+    # Change target to a binary classification
+    target = LabelEncoder().fit_transform(target)
+
 #split the data into training/test
 #normalize each attribute
 
@@ -29,8 +37,10 @@ def evaluate(bayes_results, svm_results):
 #signed distance from hyperplane for SVM? pg 554
 
 if __name__ == '__main__':
-    visualize_data()
-    processed_data = preprocess()
-    bayes_results = run_bayes(processed_data)
-    svm_results = run_SVM(processed_data)
-    evaluate(bayes_results, svm_results)
+    filename = "magic.data"
+    raw_magic = pd.read_csv(filename, header=None, skipinitialspace=True)
+    visualize_data(raw_magic)
+    magic, test_magic = preprocess(raw_magic)
+    bayes_results = run_bayes(magic)
+    svm_results = run_SVM(magic)
+    evaluate(bayes_results, svm_results, test_magic)
