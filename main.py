302cfd7ec777167c1c3b8fab68c468c912f6f67b
Bryce Elizabeth Yahn <byahn2@u.rochester.edu>
2020-11-08 21:17:47 -0500

    split into test and train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
 
 #Bryce will be responsible for preprocessing and coding/running SVM.
 #Jeremy will be responsible for: visualization, coding/running Bayes.
@@ -13,24 +14,26 @@ def visualize_data(data):
 
 #DATA PREPROCESSING
 def preprocess(raw_data):
+    # split the data into training/test
+    # normalize each attribute
     target_index = len(raw_data.columns)-1
     attributes = raw_data.drop(target_index, axis=1)
     target = raw_data[target_index]
     # Change target to a binary classification
     target = LabelEncoder().fit_transform(target)
+    X_train, X_test, y_train, y_test = train_test_split(attributes, target, test_size=0.25, shuffle=True)
 
-#split the data into training/test
-#normalize each attribute
+
 
 #Run Naive Bayes
-def run_bayes(data):
+def run_bayes(X,y):
     print('run naive Bayes')
 #Run SVM
-def run_SVM(data):
+def run_SVM(X,y):
     print('run SVM')
 
 #EVAL
-def evaluate(bayes_results, svm_results):
+def evaluate(bayes_results, svm_results, test_X, test_y):
     print('evaluate')
 #area under ROC for comparing them to eachother
 #posterior probability for Bayes?
@@ -38,9 +41,9 @@ def evaluate(bayes_results, svm_results):
 
 if __name__ == '__main__':
     filename = "magic.data"
-    raw_magic = pd.read_csv(filename, header=None, skipinitialspace=True)
-    visualize_data(raw_magic)
-    magic, test_magic = preprocess(raw_magic)
-    bayes_results = run_bayes(magic)
-    svm_results = run_SVM(magic)
-    evaluate(bayes_results, svm_results, test_magic)
+    magic = pd.read_csv(filename, header=None, skipinitialspace=True)
+    visualize_data(magic)
+    X, y, test_X, test_y = preprocess(magic)
+    bayes_results = run_bayes(X,y)
+    svm_results = run_SVM(X,y)
+    evaluate(bayes_results, svm_results, test_X, test_y)
