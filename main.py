5e3bddbd71997e38aad827ead53c3ebbc2ef8826
Bryce Elizabeth Yahn <byahn2@u.rochester.edu>
2020-11-18 20:47:00 -0500

    Kind of added the standardization for Cauchy distribution, but need to double check

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from sklearn.preprocessing import LabelEncoder, scale
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn import svm, metrics
+from scipy.stats import cauchy
 
 
 
@@ -19,8 +20,11 @@ def preprocess(raw_data):
     # Change target to a binary classification
     target = LabelEncoder().fit_transform(target)
     # gamma = 0, hadron = 1
+    norm_att = [0,1,2,3,4,5,6,7,8]
+    cauchy_att = [9]
     #normalize all attributes (may change how we do this)
-    attributes = scale(attributes)
+    attributes[norm_att] = scale(attributes[norm_att])
+    attributes[cauchy_att] = cauchy.pdf(attributes[cauchy_att], 0, 1)
     #split data into train and test set .25 to .75
     X_train, X_test, y_train, y_test = train_test_split(attributes, target, test_size=0.25, shuffle=True)
     return X_train, X_test, y_train, y_test
@@ -36,14 +40,14 @@ def run_SVM(X,y):
     # https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html#sklearn.svm.SVC
     # https://scikit-learn.org/stable/modules/svm.html#svm-kernels
     # I'm going to look more into the details of how this works and what parameters I can use
-    # kernel can be linear, polynomial, rbf, or sigmoid
     clf = svm.SVC(C=1, kernel='rbf', gamma='scale')
     clf.fit(X, y)
     return clf
 
 #EVAL
 def evaluate(bayes_model, svm_model, X, y):
-    print('evaluate')
+    #print('evaluate Naive Bayes')
+    print('evaluate SVM')
     y_svm = y.copy()
     y_svm[y_svm == 0] = -1
     svm_result = svm_model.predict(X)
