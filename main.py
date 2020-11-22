3626827e535544b0bf706e59fb22457588f60f28
Jeremy Atkins <jatkins5@u.rochester.edu>
2020-11-22 17:19:43 -0500

    minmax scaled data

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import LabelEncoder, scale, PowerTransformer
+from sklearn.preprocessing import LabelEncoder, scale, PowerTransformer, MinMaxScaler
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn import svm, metrics
@@ -15,7 +15,7 @@ from scipy.stats import cauchy, skew
 #We will both be contributing to the presentation, report, proofreading, checking, helping each other debug.
 
 #DATA PREPROCESSING
-def preprocess(raw_data, scalepls=True):
+def preprocess(raw_data, gaussianscale=True):
     """
     raw_data: raw dataframe imported from csv
     """
@@ -32,32 +32,34 @@ def preprocess(raw_data, scalepls=True):
     attributes[norm_att] = scale(attributes[norm_att])
     attributes[cauchy_att] = cauchy.pdf(attributes[cauchy_att], 0, 1)
     """
-    rescaled_df = None
-    if scalepls:
-        rescaled_df = scale(attributes)
-    else:
-        rescaled_df = attributes
+    rescaled_df = scale(attributes, gaussianscale=gaussianscale)
     #split data into train and test set .25 to .75
     X_train, X_test, y_train, y_test = train_test_split(rescaled_df, target, test_size=0.25, shuffle=True)
     return X_train, X_test, y_train, y_test
 
     
 
-def scale(attr_df):
+def scale(attr_df, gaussianscale=True):
     """
     attr_df: dataframe containing only attributes
     """
     skew_threshold = 0.5
 
+    mm_scaler = MinMaxScaler()
+    scaled_xdf = mm_scaler.fit_transform(attr_df)
+
+    if not gaussianscale:
+        return scaled_xdf
+
     # work out which attributes are skewed
     nonskewed_cols = []
-    for col in attr_df.columns:
-        thisskew = skew(attr_df[col])
+    for col in scaled_xdf.columns:
+        thisskew = skew(scaled_xdf[col])
         #print(col, thisskew)
         if np.abs(thisskew) < skew_threshold:
             nonskewed_cols.append(col)
     nonskewed_cols.append("fAlpha") # drop the fAlpha because it's weird
-    skew_xdf = attr_df.drop(nonskewed_cols, axis=1) # this df contains only the skewed attributes
+    skew_xdf = scaled_xdf.drop(nonskewed_cols, axis=1) # this df contains only the skewed attributes
 
     # do the actual unskew
     pt = PowerTransformer(method="yeo-johnson", standardize=True)
@@ -66,11 +68,11 @@ def scale(attr_df):
         columns=skew_xdf.columns,
     )
 
-    # remerge dataframes
     skewed_cols = [col for col in attr_df.columns if col not in nonskewed_cols]
     nonskew_xdf = attr_df.drop(skewed_cols, axis=1)
 
-    merged_df = pd.concat([nonskew_xdf, unskew_xdf], axis=1)
+    # remerge dataframes
+    merged_df = pd.concat([scaled_xdf, unskew_xdf], axis=1)
 
     #TODO: work out what to do with "fAlpha" (powerlaw? KDE? see work in nb notebook)
     # for now nothing haha
@@ -146,43 +148,6 @@ def evaluate(nb_model, svm_model, X, y):
     prfig.savefig("prfig.png", dpi=300)
 
 
-    """
-    svm_result = svm_model.decision_function(X)
-    fpr_svm, tpr_svm, _ = metrics.roc_curve(y_svm, svm_result)
-    auc_svm = metrics.roc_auc_score(y_svm, svm_result)
-    precision_svm, recall_svm, _ = metrics.precision_recall_curve(y_svm, svm_result)
-    pr_auc_svm = metrics.auc(recall_svm, precision_svm)
-    print('Area under ROC curve for SVM: ', auc_svm)
-    print('Area under Precision-Recall curve for SVM: ', pr_auc_svm)
-
-    nb_result = nb_model.predict_proba(X)
-    fpr_nb, tpr_nb, _ = metrics.roc_curve(y, nb_result[:,1])
-    auc_nb = metrics.roc_auc_score(y, nb_result[:,1])
-    precision_nb, recall_nb, _ = metrics.precision_recall_curve(y, nb_result[:,1])
-    pr_auc_nb = metrics.auc(recall_nb, precision_nb)
-    print('\nArea under ROC curve for NB: ', auc_nb)
-    print('Area under Precision-Recall curve for NB: ', pr_auc_nb)
-
-    fig, axs = plt.subplots(2,2)
-    axs[0,0].plot(fpr_nb, tpr_nb, marker='.', label='ROC AUC = %0.2f' % auc_nb)
-    axs[0,0].set_title('ROC curve for NB')
-    axs[0,0].set(xlabel='False Positive Rate', ylabel='True Positive Rate')
-    axs[0,0].legend(loc='lower right')
-    axs[0,1].plot(fpr_svm, tpr_svm, marker='.', label='ROC AUC = %0.2f' % auc_svm)
-    axs[0,1].set_title('ROC curve for SVM')
-    axs[0,1].set(xlabel='False Positive Rate', ylabel='True Positive Rate')
-    axs[0,1].legend(loc='lower right')
-    axs[1,0].plot(precision_nb, recall_nb, marker='.', label='PR AUC = %0.2f' % pr_auc_nb)
-    axs[1,0].set_title('P-R curve for NB')
-    axs[1,0].set(xlabel='Precision', ylabel='Recall')
-    axs[1,0].legend(loc='lower left')
-    axs[1,1].plot(precision_svm, recall_svm, marker='.', label='PR AUC = %0.2f' % pr_auc_svm)
-    axs[1,1].set_title('P-R curve for SVM')
-    axs[1,1].set(xlabel='Precision', ylabel='Recall')
-    axs[1,1].legend(loc='lower left')
-    plt.show()
-    """
-
 #area under ROC for comparing them to eachother
 #posterior probability for Bayes?
 #signed distance from hyperplane for SVM? pg 554
@@ -203,8 +168,8 @@ def main():
         "class",
     )
     magic = pd.read_csv(filename, header=None, skipinitialspace=True, names=cols)
-    X_sc, X_test_sc, y_sc, y_test_sc = preprocess(magic)
-    X, X_test, y, y_test = preprocess(magic, scalepls=False)
+    #X_sc, X_test_sc, y_sc, y_test_sc = preprocess(magic)
+    X, X_test, y, y_test = preprocess(magic, gaussianscale=False)
     bayes_model = run_bayes(X,y)
     svm_model = run_SVM(X,y)
     evaluate(bayes_model, svm_model, X_test, y_test)
